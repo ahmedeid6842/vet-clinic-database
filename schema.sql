b748757ca64aef31a20aa80840333c22baefaa98
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-16 01:03:14 +0300

    change the animal table schema - add species

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
+-- Create table animals
 CREATE TABLE animals (
   id INT,
   name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL(10,2)
+  weight_kg DECIMAL(10,2),
+  species VARCHAR(255)
 );
