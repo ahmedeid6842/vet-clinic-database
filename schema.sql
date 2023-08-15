25bc8684b92c226d2c50e1a28cb7a43dfa892537
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-16 02:37:16 +0300

    Using alter to add a new column

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@ CREATE TABLE animals (
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL(10,2),
-  species VARCHAR(255)
+  weight_kg DECIMAL(10,2)
 );
+
+-- Add a new column to animal table using alter
+ALTER TABLE animals ADD species varchar(255);
