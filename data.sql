576aeb4fe18a98e4126b2b9a4157b369e8830912
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-16 22:51:52 +0300

    Insert data to ownsers and species

--- data.sql
+++ data.sql
@@ -14,3 +14,17 @@ VALUES
   (9, 'Boarmon', '2005-06-07', 7, true, 20.4, NULL),
   (10, 'Blossom', '1998-10-13', 3, true, 17, NULL),
   (11, 'Ditto', '2022-05-14', 4, true, 22, NULL);
+
+-- Insert data into the owners table
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- Insert data into the species table
+INSERT INTO species (name) VALUES
+  ('Pokemon'),
+  ('Digimon');
