7464db266b047ef611f8329ad671eb8500e546d1
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-17 20:59:04 +0300

    insert data to specializations table

--- data.sql
+++ data.sql
@@ -53,3 +53,10 @@ VALUES ('William Tatcher', 45, '2000-04-23'),
        ('Maisy Smith', 26, '2019-01-17'),
        ('Stephanie Mendez', 64, '1981-05-04'),
        ('Jack Harkness', 38, '2008-06-08');
+
+-- Insert the following data for specializations:
+INSERT INTO specializations (vet_id, species_id)
+VALUES (1, 1),
+       (4, 2),
+       (3, 1),
+       (3, 2);
